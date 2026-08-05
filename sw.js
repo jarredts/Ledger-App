1ceb5377cb6b56f1e1911a17c2ec7041a1598228
jarredts <jarredts@gmail.com>
2026-08-05 04:08:16 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'ledger-cache-v19';
-const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'ledger-cache-v20';
+const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './notification-badge.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
